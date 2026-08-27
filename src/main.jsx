f4b4de8b291d58ddfab6df150cf57aafb9c4851f
obeidatshadi-max <shadi@psychologytobusiness.com>
2026-08-27 18:12:34 +0300

    feat: route /manager to the team dashboard app

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import RaterApp from './RaterApp.jsx';
+import ManagerApp from './ManagerApp.jsx';
 import { supabaseAuthAdapter } from './lib/authAdapter.js';
 import { LanguageProvider } from './i18n/LanguageContext.jsx';
 import './styles/global.css';
 
 const raterMatch = window.location.pathname.match(/^\/rate\/([0-9a-f-]{36})$/i);
+const isManagerRoute = window.location.pathname === '/manager';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LanguageProvider>
       {raterMatch
         ? <RaterApp linkId={raterMatch[1]} authAdapter={supabaseAuthAdapter} />
+        : isManagerRoute
+        ? <ManagerApp authAdapter={supabaseAuthAdapter} />
         : <App authAdapter={supabaseAuthAdapter} />}
     </LanguageProvider>
   </React.StrictMode>
